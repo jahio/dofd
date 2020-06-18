4d50fa28b0627d10c01bdcc257192666ad489db8
J. Austin Hughey <jah@jah.io>
2020-06-18 13:21:02 -0600

    rspec spec/requests works

--- railsapp/app/controllers/posts_controller.rb
+++ railsapp/app/controllers/posts_controller.rb
@@ -69,6 +69,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.fetch(:post, {})
+      params.require(:post).permit(:title, :body)
     end
 end
