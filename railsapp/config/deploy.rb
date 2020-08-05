65304b09aa218035c7b1d6bf5b5237ccb11d95c5
Ubuntu <ubuntu@ip-172-31-20-247.ap-south-1.compute.internal>
2020-08-05 12:11:20 +0000

    changed deploy.rb

--- railsapp/config/deploy.rb
+++ railsapp/config/deploy.rb
@@ -4,8 +4,9 @@ require 'mina/git'
 require 'mina/rbenv'
 
 
-set :domain,     '127.0.0.1'
-set :deploy_to,  "/home/ubuntu/app"
+set :domain,  '13.233.112.15'
+set :user, 'jenkins'
+set :deploy_to, "/home/ubuntu/app"
 set :app_path,   "/home/ubuntu/app/current"
 set :repository, 'https://github.com/bhagyesh27/dofd.git'
 set :branch,     'master'
